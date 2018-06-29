3a760840e9102ef3d727484b0c76fd1e8bec8da5
domenzero <merkulovmx@gmail.com>
2018-06-29 16:01:48 +0400

    %Print href link and compare%

--- csssr_test/csssr_tests.py
+++ csssr_test/csssr_tests.py
@@ -1,7 +1,6 @@
 import os
 import unittest
 import urllib.request
-import re
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -16,54 +15,41 @@ class csssrTests(unittest.TestCase):
 		self.url = "https://csssr.github.io/qa-engineer"
 		self.driver.implicitly_wait(20)
 
-# 	def test_percent_5_0(self):
-# 		driver=self.driver
-# 		driver.get("https://csssr.github.io/qa-engineer")
-
-# # Click tabs
-# 		driver.find_element_by_partial_link_text("НАХОДИТЬ НЕСОВЕРШЕНСТВА").click()
-# 		sleep(3)
-# 		driver.find_element_by_partial_link_text("СОПРОВОЖДАТЬ ПРОЕКТЫ").click()
-# 		sleep(3)
-# 		driver.find_element_by_partial_link_text("РАБОТАТЬ С ФАЙЛАМИ ПРОЕКТОВ").click()
-# 		sleep(3)
-# 		driver.find_element_by_partial_link_text("ВНИКАТЬ В ДЕТАЛИ ПРОЕКТОВ").click()
-# 		sleep(3)
-
-	def test_link_8_0(self):
+	def test_percent_5_0(self):
+		driver=self.driver
+		driver.get("https://csssr.github.io/qa-engineer")
 
 # Click tabs
+		driver.find_element_by_partial_link_text("НАХОДИТЬ НЕСОВЕРШЕНСТВА").click()
+		sleep(3)
+		driver.find_element_by_partial_link_text("СОПРОВОЖДАТЬ ПРОЕКТЫ").click()
+		sleep(3)
+		driver.find_element_by_partial_link_text("РАБОТАТЬ С ФАЙЛАМИ ПРОЕКТОВ").click()
+		sleep(3)
+		driver.find_element_by_partial_link_text("ВНИКАТЬ В ДЕТАЛИ ПРОЕКТОВ").click()
+		sleep(3)
+
+	def test_link_8_0(self):
 		url_list0 = []
 		html_page = urllib.request.urlopen(self.url)
 		soup = BeautifulSoup(html_page, "lxml")
-		# for id_1 in soup.find_all("a"):
-		
 
-		# for tag in soup.find_all(re.compile("http[s]?^")):
-		# 	print(tag.name)
-
+# We're searching link
 		link=soup.find("input",id="soft").find_next("a")
 
-		# print(link.find_next("a"))
-		print(link["href"])
+# We're printing a current link
+		print("_______8_0 Test link_______ ")
+		print("It's link: "+link["href"])
 
-		# for link1 in link.find_next_siblings("a"):
-		# 	  if "app" in link1["href"]:
-		# 	  	print('found a url with national-park in the link')
-
-		# print(link.find_next("a"))
-		 	# print(id_1["data-id"])
-		 	# url_list0.append(id_1["href"])
-		 	# print("bala: "+id_1["type"])
-		 	# print(id_1.find_next_siblings("a"))
-		 	# print(id_1.p['a'])
-
-# print the first elemnt id 
-		# print("bala: "+url_list0[0]) 
-
-    
+# And print test result if link searched
+		if "http://monosnap.com/" in link["href"]:
+			print('Pass!')
+		else:
+		  	print('Fail! It is not this link: http://monosnap.com/' )
+   
 	def tear_down(self):
 		self.driver.quit()
+		driver.guit()
  
 if __name__ == "__main__":
 	unittest.main()
